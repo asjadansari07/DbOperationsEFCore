461875b6f0c267bca758a25cc8db5619f1f76c17
Asjed Ansari <asjadansari07@gmail.com>
2026-07-09 17:02:05 +0530

    added master tables with data

--- DbOperationsEFCore/Program.cs
+++ DbOperationsEFCore/Program.cs
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
 );
@@ -17,6 +20,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    // Enable Swagger
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
