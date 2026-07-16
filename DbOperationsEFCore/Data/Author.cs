e4f60f84e884a4fb08e6af761683068a97cc389b
Asjed Ansari <asjadansari07@gmail.com>
2026-07-17 03:03:16 +0530

    insert into multiple tables

---
+++ DbOperationsEFCore/Data/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace DbOperationsEFCore.Data
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
--- DbOperationsEFCore/Data/Book.cs
+++ DbOperationsEFCore/Data/Book.cs
@@ -10,8 +10,10 @@
         public DateTime CreatedOn { get; set; }
 
         public int LanguageId { get; set; }
+        public int? AuthorId { get; set; }
 
         public Language? Language { get; set; }
+        public Author? Author { get; set; }
 
 
     }
